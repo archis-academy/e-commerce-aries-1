f43df2229baaebc6a2c39df61d54dfe42ec27f04
slhoz <lgsalih@gmail.com>
2024-04-15 11:58:25 +0300

    Tüm butonlar çalışır hale getirildi, Add To Cart sistemine remove eklendi.

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ function renderExploreProducts() {
 <div class="image-container card">
 <img class="product-image" src="${product.image}" alt="${product.title}" />
   <div class="overlay">
-            <div class="text" onclick="addToCart(${
-              product.id
-            })">Add to Cart</div>
+            <div class="text" id="addToCart_${product.id}" onclick="addToCart(${
+        product.id
+      })">Add To Cart</div>
         </div>
 </div>
 <h3 class="product-title">  ${productText(product.title)}</h3>
@@ -64,6 +64,7 @@ function addToCart(productId) {
   const cartItems = JSON.parse(localStorage.getItem("cartProducts")) || [];
 
   const isAdded = cartItems.some((product) => product.id === productId);
+  const addButton = document.querySelector(`#addToCart_${productId}`);
 
   if (!isAdded) {
     const productToAdd = allProducts.find(
@@ -74,13 +75,38 @@ function addToCart(productId) {
       "cartProducts",
       JSON.stringify([...cartItems, productToAdd])
     );
+    addButton.textContent = "Remove From Cart";
   } else {
-    deleteCartProduct(productId);
+    deleteCartProduct(productId, cartItems);
+    addButton.textContent = "Add To Cart";
   }
 }
 
-function deleteCartProduct(productId) {
-  // filter methodunu kullanarak productId si eşleşen ürünü çıkarıyoruz.
+function deleteCartProduct(productId, cartItems) {
+  const filteredItems = cartItems.filter((product) => product.id !== productId);
+
+  localStorage.setItem("cartProducts", JSON.stringify(filteredItems));
 }
 
+function viewAll() {
+  exploreEnd = allProducts.length;
+  exploreStart = 0;
+  renderExploreProducts();
+}
+
+function nextBtn() {
+  if (exploreEnd < allProducts.length) {
+    exploreStart += 8;
+    exploreEnd += 8;
+    renderExploreProducts();
+  }
+}
+
+function backBtn() {
+  if (exploreStart > 0) {
+    exploreStart -= 8;
+    exploreEnd -= 8;
+    renderExploreProducts();
+  }
+}
 /* SALIH HOMEPAGE EXPLORE PRODUCTS END */
